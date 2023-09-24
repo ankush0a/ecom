a3082b143bfcd41c1a9cb1e87b3ffac7964679f6
ankush <ankush>
2023-09-24 21:03:14 +0530

    update backend layout

--- resources/views/backend/dashboard/admin.blade.php
+++ resources/views/backend/dashboard/admin.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
   <div class="container-fluid flex-grow-1 container-p-y">
-    <h4 class="fw-bold py-3 mb-4">Page 1</h4>
-    <p>
-      Sample page.<br />For more layout options refer
-      <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation//layouts.html" target="_blank" class="fw-bold">Layout docs</a>.
-    </p>
+    <h4 class="fw-bold py-3 mb-4">Dashboard</h4>
   </div>
 @endsection
--- resources/views/layouts/backend/footer.blade.php
+++ resources/views/layouts/backend/footer.blade.php
@@ -1,11 +1,9 @@
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-fluid">
       <div class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
+        <div>{{ env('APP_NAME') }} © {{ date('Y') }}</div>
         <div>
-          © 2023, made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="fw-semibold">Pixinvent</a>
-        </div>
-        <div>
-          <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
+          <a href="javascript:void(0);" target="_blank" class="footer-link me-4">Developer</a>
         </div>
       </div>
     </div>
