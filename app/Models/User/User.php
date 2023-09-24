e722ee8fb497a799fadc4767531a4aa17f7b2685
ankush <ankush>
2023-09-24 20:34:47 +0530

    added sluggable

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -2,44 +2,50 @@
 
 namespace App\Models\User;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\{HasMany};
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Sluggable\{HasSlug, SlugOptions};
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasSlug;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * Get the options for generating the slug.
+   */
+  public function getSlugOptions(): SlugOptions
+  {
+    return SlugOptions::create()->generateSlugsFrom(['first_name', 'last_name'])->saveSlugsTo('slug');
+  }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = ["first_name", "last_name", "email", "mobile", "role", "password", "profile", "permission"];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = ['password', 'remember_token'];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = ['email_verified_at' => 'datetime', 'password' => 'hashed'];
+
+  public function usermeta(): HasMany
+  {
+    return $this->hasMany(Usermeta::class);
+  }
 }
